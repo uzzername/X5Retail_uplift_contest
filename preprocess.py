b244ae0b1aa815c5307a48d9f412b625791035c4
Eugene Burzilov <eugene.burzilov@gmail.com>
2024-08-21 19:43:56 +0300

    init preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,6 @@
+# preprocess raw data for feature pandas DataFrames
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='data')
